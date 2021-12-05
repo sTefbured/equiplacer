2a813b1f33d4caa0e05b6143c95bfaec35666cbf
sTefbured <stef.for.ever2000@gmail.com>
2021-12-05 22:06:19 +0300

    #8 Implement missing "Income" button.

--- equiplacer-desktop-client/src/main/java/com/kotikov/equiplacer/client/desktop/view/panel/tabcontent/input/InputPanel.java
+++ equiplacer-desktop-client/src/main/java/com/kotikov/equiplacer/client/desktop/view/panel/tabcontent/input/InputPanel.java
@@ -1,6 +1,7 @@
 package com.kotikov.equiplacer.client.desktop.view.panel.tabcontent.input;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class InputPanel extends JPanel {
     private static final String[] TABLE_HEADERS = new String[]{
@@ -19,12 +20,10 @@ public class InputPanel extends JPanel {
 
     public InputPanel(JTabbedPane parentTabbedPane) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-
         add(new EquipmentDetailsPanel());
-
-        costsTable = new JTable(new Object[][]{{"1", "2", "3", "4"}}, TABLE_HEADERS);
+        add(Box.createRigidArea(new Dimension(0, 5)));
+        costsTable = new JTable(new Object[][]{{"1", "2", "3", "4"}}, TABLE_HEADERS); //TODO: delete when table logics are implemented
         add(new JScrollPane(costsTable));
-
         var calculateButtonWrapper = new JPanel();
         calculateButtonWrapper.add(calculateButton);
         add(calculateButtonWrapper);
