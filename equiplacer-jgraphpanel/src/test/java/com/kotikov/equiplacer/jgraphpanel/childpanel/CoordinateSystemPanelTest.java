094c8a78eae7842f6e9d858ea6899d5fd42dabc1
sTefbured <stef.for.ever2000@gmail.com>
2021-12-08 03:11:22 +0300

    #17 Migrate equiplacer-jgraphpanel (jbinarytreepanel) to use Graph.

---
+++ equiplacer-jgraphpanel/src/test/java/com/kotikov/equiplacer/jgraphpanel/childpanel/CoordinateSystemPanelTest.java
@@ -0,0 +1,33 @@
+package com.kotikov.equiplacer.jgraphpanel.childpanel;
+
+import com.kotikov.equiplacer.graph.Graph;
+import com.kotikov.equiplacer.jgraphpanel.node.NodeComponent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+class CoordinateSystemPanelTest {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(500, 500);
+            frame.setLocationRelativeTo(null);
+            frame.setLayout(new GridLayout(1, 1));
+
+            var adjacencyMatrix = new int[][]{
+                    {0, 1, 1, 0},
+                    {1, 0, 0, 1},
+                    {1, 0, 0, 0},
+                    {0, 1, 0, 0}
+            };
+            var data = List.of(new NodeComponent<>(3, 30),
+                    new NodeComponent<>(4, 30), new NodeComponent<>(1, 30), new NodeComponent<>(1, 30));
+            Graph<NodeComponent<Integer>> graph = new Graph<>(adjacencyMatrix, data);
+
+            frame.add(new CoordinateSystemPanel<>(graph));
+            frame.setVisible(true);
+        });
+    }
+}
